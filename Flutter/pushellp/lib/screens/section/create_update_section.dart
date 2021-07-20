1c040a88a5d193365b38c52e64ca5955b147dac3
Ludovic FAVIER <lfavier@live.fr>
2021-07-20 18:11:26 +0200

    begin create_update section

--- Flutter/pushellp/lib/screens/section/create_update_section.dart
+++ Flutter/pushellp/lib/screens/section/create_update_section.dart
@@ -4,20 +4,30 @@ import 'package:pushellp/commun/back_btn_custom.dart';
 import 'package:pushellp/commun/drawerCustom.dart';
 import 'package:pushellp/models/User.dart';
 
-class CreateUpdateSection extends StatelessWidget {
+class CreateUpdateSection extends StatefulWidget {
   final User user;
   final bool isCreatePage;
+
   const CreateUpdateSection({Key? key, required this.user, required this.isCreatePage}) : super(key: key);
+
+  @override
+  _CreateUpdateSectionState createState() => _CreateUpdateSectionState();
+}
+
+class _CreateUpdateSectionState extends State<CreateUpdateSection> {
+  final _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+  //final _descriptionController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarCustom(
         title: "Manage Sections",
-        userPseudo: user.pseudo,
+        userPseudo: widget.user.pseudo,
       ),
       drawer: DrawerCustom(
-        user: user,
+        user: widget.user,
       ),
       body: SafeArea(
         child: Column(
@@ -28,7 +38,7 @@ class CreateUpdateSection extends StatelessWidget {
               child: Align(
                 alignment: Alignment.centerLeft,
                 child: BackButtonCustom(
-                  user: user,
+                  user: widget.user,
                   btnLeadsToHomePage: false,
                 ),
               ),
@@ -39,15 +49,47 @@ class CreateUpdateSection extends StatelessWidget {
                 child: Column(
                   children: [
                     Text(
-                      isCreatePage ? "Create a section" : "Update a section",
+                      widget.isCreatePage ? "Create a section" : "Update a section",
                       style:
                           TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
                     ),
                     SizedBox(height: 15),
+                    createForm()
                   ],
                 ),
               ),
             )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Form createForm(){
+    return Form(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      key: _formKey,
+      child: Container(
+        width: MediaQuery.of(context).size.width / 2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              child: TextFormField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  hintText: "Title",
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter the title";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            SizedBox(height: 15),
           ],
         ),
       ),
